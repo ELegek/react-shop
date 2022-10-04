1427c444d355ea58daeca776d60bf4a5c0d0ee07
Evgeny Lezhepekov <evgeny.legek@yandex.ru>
2022-10-04 19:22:51 +0300

    create component item

--- src/App.js
+++ src/App.js
@@ -1,12 +1,61 @@
 import React from 'react';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import Items from './components/Items';
 
 class App extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			items: [
+				{
+					id: 1,
+					title: 'Стул сурый',
+					img: 'chair-grey.jpg',
+					desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing.',
+					category: 'chairs',
+					price: '49.99',
+				},
+				{
+					id: 2,
+					title: 'Стол',
+					img: 'table.jpeg',
+					desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing.',
+					category: 'tables',
+					price: '149.99',
+				},
+				{
+					id: 3,
+					title: 'Диван',
+					img: 'sofa.jpeg',
+					desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing.',
+					category: 'sofa',
+					price: '549.99',
+				},
+				{
+					id: 4,
+					title: 'Лампа',
+					img: 'wall-light.jpeg',
+					desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing.',
+					category: 'light',
+					price: '25.00',
+				},
+				{
+					id: 5,
+					title: 'Стул белый',
+					img: 'chair-white.jpg',
+					desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing.',
+					category: 'chairs',
+					price: '49.99',
+				},
+			],
+		};
+	}
 	render() {
 		return (
 			<div className='wrapper'>
 				<Header />
+				<Items items={this.state.items} />
 				<Footer />
 			</div>
 		);
