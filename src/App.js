cdf4b2b46ca3cc2014a149e78922e0ddb056234c
Evgeny Lezhepekov <evgeny.legek@yandex.ru>
2022-10-04 17:19:47 +0300

    create component Header and Footer

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
+import React from 'react';
+import Header from './components/Header';
+import Footer from './components/Footer';
 
-
-function App() {
-  return 
+class App extends React.Component {
+	render() {
+		return (
+			<div className='wrapper'>
+				<Header />
+				<Footer />
+			</div>
+		);
+	}
 }
 
 export default App;
