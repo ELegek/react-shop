5de2def162121a639f4c7c9627c816e6dddc7c87
Evgeny Lezhepekov <evgeny.legek@yandex.ru>
2022-10-05 11:10:49 +0300

    create delete button in component order

--- src/App.js
+++ src/App.js
@@ -7,10 +7,11 @@ class App extends React.Component {
 	constructor(props) {
 		super(props);
 		this.state = {
+			orders: [],
 			items: [
 				{
 					id: 1,
-					title: 'Стул сурый',
+					title: 'Стул серый',
 					img: 'chair-grey.jpg',
 					desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing.',
 					category: 'chairs',
@@ -50,15 +51,33 @@ class App extends React.Component {
 				},
 			],
 		};
+		this.addToOrder = this.addToOrder.bind(this);
+		this.deleteOrder = this.deleteOrder.bind(this);
 	}
 	render() {
 		return (
 			<div className='wrapper'>
-				<Header />
-				<Items items={this.state.items} />
+				<Header orders={this.state.orders} onDelete={this.deleteOrder} />
+				<Items items={this.state.items} onAdd={this.addToOrder} />
 				<Footer />
 			</div>
 		);
+	}
+
+	deleteOrder(id) {
+		this.setState({ orders: this.state.orders.filter((el) => el.id !== id) });
+	}
+
+	addToOrder(item) {
+		let isInArray = false;
+		this.state.orders.forEach((el) => {
+			if (el.id === item.id) {
+				isInArray = true;
+			}
+		});
+		if (!isInArray) {
+			this.setState({ orders: [...this.state.orders, item] });
+		}
 	}
 }
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,29 @@
 import React, { useState } from 'react';
 import { FaShoppingCart } from 'react-icons/fa';
+import Order from './Order';
 
-export default function Header() {
+const showOrders = (props) => {
+	let summa = 0;
+	props.orders.forEach((el) => (summa += Number.parseFloat(el.price)));
+	return (
+		<div>
+			{props.orders.map((el) => (
+				<Order onDelete={props.onDelete} key={el.id} item={el} />
+			))}
+			<p className='summa'>Сумма: {new Intl.NumberFormat().format(summa)}$</p>
+		</div>
+	);
+};
+
+const showNothing = () => {
+	return (
+		<div className='empty'>
+			<h2>Товаров нет</h2>
+		</div>
+	);
+};
+
+export default function Header(props) {
 	let [cartOpen, setCartOpen] = useState(false);
 
 	return (
@@ -15,7 +37,7 @@ export default function Header() {
 				</ul>
 				<FaShoppingCart onClick={() => setCartOpen((cartOpen = !cartOpen))} className={`shop-cart-button ${cartOpen && 'active'}`} />
 
-				{cartOpen && <div className='shop-cart'></div>}
+				{cartOpen && <div className='shop-cart'>{props.orders.length > 0 ? showOrders(props) : showNothing()}</div>}
 			</div>
 			<div className='presentation'></div>
 		</header>
